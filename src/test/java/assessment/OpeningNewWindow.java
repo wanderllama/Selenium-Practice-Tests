a4d595c074491f1428b291e8d219d1fa219a1f9f
James Wallace <jawallace0392@gmail.com>
2022-09-29 17:09:38 -0400

    opening new window test

---
+++ src/test/java/assessment/OpeningNewWindow.java
@@ -0,0 +1,41 @@
+package assessment;
+
+import com.sun.source.tree.AssertTree;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.NewWindowPage;
+import pages.OpeningNewWindowPage;
+import util.Driver;
+
+import java.util.List;
+import java.util.Set;
+
+public class OpeningNewWindow extends Hooks {
+
+    OpeningNewWindowPage page = new OpeningNewWindowPage(Driver.getDriver());
+    NewWindowPage newWindowPage = new NewWindowPage(Driver.getDriver());
+
+    @Test
+    public void open_in_new_tab_test() {
+        Driver.getDriver().get("http://localhost:7080/windows");
+
+        page.openNewWindowLink.click();
+
+        String currentHandle = Driver.getDriver().getWindowHandle();
+        Set<String> handles = Driver.getDriver().getWindowHandles();
+
+        Assert.assertTrue(handles.size() != 1 , "new window not opened, getWindowHandles() only returned one handle");
+
+        for (String handle : handles) {
+            if (!handle.equals(currentHandle)) {
+                Driver.getDriver().switchTo().window(handle);
+            }
+        }
+
+        String expectedText = "New Window";
+        String actualText = NewWindowPage.headerThree.getText();
+
+        Assert.assertEquals(actualText , expectedText , "The header text of the new window is not correct. actualText: " + actualText + "\nexpectedText: " + expectedText);
+
+    }
+}

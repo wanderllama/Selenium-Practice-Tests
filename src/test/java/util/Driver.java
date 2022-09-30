02c553555f618a51ac985ef0371e5425da816df2
James Wallace <jawallace0392@gmail.com>
2022-09-29 22:02:20 -0400

    removed import i didnt need from Driver class

--- src/test/java/util/Driver.java
+++ src/test/java/util/Driver.java
@@ -1,6 +1,5 @@
 package util;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;

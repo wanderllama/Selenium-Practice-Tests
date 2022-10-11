3001434c3bd943fd56f22a56de3e9c8e6443fc4c
James Wallace <jawallace0392@gmail.com>
2022-10-10 20:07:09 -0400

    update

--- src/test/java/assessment/LoginValidation.java
+++ src/test/java/assessment/LoginValidation.java
@@ -1,5 +1,7 @@
 package assessment;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;

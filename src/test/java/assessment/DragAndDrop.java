b0f352c70715e8537319485814c99c3e5eaf490a
James Wallace <jawallace0392@gmail.com>
2022-10-14 18:52:08 -0400

    added surefire plugin to run from jenkins

--- src/test/java/assessment/DragAndDrop.java
+++ src/test/java/assessment/DragAndDrop.java
@@ -14,11 +14,15 @@ public class DragAndDrop extends Driver{
     public void drag_and_drop_test() throws InterruptedException {
         Driver.getDriver().get("http://localhost:7080/drag_and_drop");
 
+        // drag and drop using the actions class was not functioning correctly
+//        Driver.getActions().dragAndDrop(page.boxA, page.boxB).build().perform();
+
 //        String originalTextA = page.boxA.getText();
         String originalTextB = page.boxB.getText();
-
+//
+        // found javascript executor approach to drag and drop
         UI_Util.move_elements(page.boxA , page.boxB);
-
+//
         String newTextA = page.boxA.getText();
 //        String newTextB = page.boxB.getText();
 
